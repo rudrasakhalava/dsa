add7ed46706dfda221dc8edd0f17c71f7a5a5102
Rudra Sakhalava <rudrasakhalava@gmail.com>
2026-07-07 22:48:30 +0530

    solve more problems

---
+++ two_pointers_array/7_two_sum.py
@@ -0,0 +1,69 @@
+"""
+Bonus Challenge (Original Problem)
+Pair With Exact Difference (Medium)
+
+This is a nice two-pointer problem that isn't just another sum variation.
+
+Problem
+
+You are given a sorted array of integers and an integer k.
+
+Return whether there exists a pair (i, j) such that:
+
+nums[j] - nums[i] == k
+
+where
+
+i < j
+
+Example
+
+Input
+
+nums = [1,3,5,8,10]
+k = 5
+
+Output
+
+True
+
+Because
+
+8 - 3 = 5
+
+Example
+
+Input
+
+nums = [2,4,7,10]
+k = 6
+
+Output
+
+True
+
+Because
+
+10 - 4 = 6
+
+"""
+
+nums = [2,4,7,10]
+k = 5
+
+i = 0
+j = len(nums) - 1
+
+while i < j:
+    dif = nums[j] - nums[i]
+
+    if dif == k:
+        print(True)
+        break
+
+    elif dif > k:
+        i += 1
+    else :
+        j -= 1
+else:
+    print(False)
