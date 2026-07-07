9a463c2b3761cfa3870a30688ecbb60e4b28aece
Rudra Sakhalava <rudrasakhalava@gmail.com>
2026-07-07 22:16:17 +0530

    Solving some new problems in dsa

---
+++ two_pointers_array/3_two_sum.py
@@ -0,0 +1,42 @@
+"""
+Problem
+
+You are given a sorted array nums and an integer target.
+
+Return the maximum sum of two numbers that is strictly less than target.
+
+If no such pair exists, return -1.
+
+Example
+Input:
+nums = [1,3,4,7,10]
+target = 15
+
+Output:
+14
+
+Explanation
+
+4 + 10 = 14
+
+It is the largest sum smaller than 15.
+"""
+
+nums = [1,3,4,7,10]
+target = 15
+
+i = 0
+j = len(nums)-1
+
+temp = -1
+
+while i < j:
+    sum = nums[i] + nums[j]
+
+    if sum < target and sum > temp:
+        temp = sum
+        i += 1
+    else:
+        j -= 1
+
+print(temp)        
---
+++ two_pointers_array/4_two_sum.py
@@ -0,0 +1,56 @@
+"""
+Problem 2: Count Pairs Smaller Than Target (Easy-Medium)
+Problem
+
+Given a sorted array nums and an integer target.
+
+Count how many pairs (i, j) satisfy
+
+i < j
+nums[i] + nums[j] < target
+
+Example
+
+Input
+
+nums = [1,2,3,4,5]
+target = 7
+
+Output
+
+6
+
+Explanation
+
+Pairs are
+
+(1,2)
+(1,3)
+(1,4)
+(1,5)
+(2,3)
+(2,4)
+
+Constraint
+
+O(n)
+"""
+
+nums = [1,2,3,4,5]
+target = 7
+
+i = 0
+j = len(nums)-1
+
+temp = 0
+
+while i < j:
+    sum = nums[i] + nums[j]
+
+    if sum < target:
+        temp += (j-i)
+        i += 1
+    elif sum >= target :
+        j -= 1
+
+print(temp)
