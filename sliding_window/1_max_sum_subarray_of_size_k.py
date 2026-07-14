0b1c8c7d4d5b3e7da792ec7f6196b56975b39593
Rudra Sakhalava <rudrasakhalava@gmail.com>
2026-07-14 18:36:16 +0530

    Learning sliding window concept

---
+++ sliding_window/1_max_sum_subarray_of_size_k.py
@@ -0,0 +1,42 @@
+"""
+
+Max Sum Subarray of size K
+
+Given an array of integers arr[]  and a number k. Return the maximum sum of a subarray of size k.
+
+Note: A subarray is a contiguous part of any given array.
+
+Examples:
+
+Input: arr[] = [100, 200, 300, 400], k = 2
+Output: 700
+Explanation: arr2 + arr3 = 700, which is maximum.
+Input: arr[] = [1, 4, 2, 10, 23, 3, 1, 0, 20], k = 4
+Output: 39
+Explanation: arr1 + arr2 + arr3 + arr4 = 39, which is maximum.
+Input: arr[] = [100, 200, 300, 400], k = 1
+Output: 400
+Explanation: arr3 = 400, which is maximum.
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 106
+1 ≤ k ≤ arr.size()
+
+"""
+
+nums = [1,5,4,2,9,9,9]
+k = 3
+
+window_sum = 0
+
+for i in range(k):
+    window_sum += nums[i]
+
+max_sum = window_sum
+for j in range(k,len(nums)):
+    window_sum += nums[j]
+    window_sum -= nums[j-k]
+
+    max_sum = max(max_sum,window_sum)
+
+print(max_sum)
